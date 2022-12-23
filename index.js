c957861313c08e95ff60d11c31e49663f2c9d8f1
vlharris1 <73183662+vlharris1@users.noreply.github.com>
2022-12-23 17:50:21 -0500

    JavaScript function with three parameters

---
+++ index.js
@@ -0,0 +1,7 @@
+function myFunction(name, height, country) {
+    console.log(name + 'is ' + height + ' inches tall and is from ' + country);
+}
+
+myFunction('Abbe ', '65', 'America.');
+
+//function with three parameters
